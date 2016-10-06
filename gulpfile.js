9ceac17632fac0dad69abbe179d540c19dcacaea
spmpl <podarimne1@gmail.com>
2016-10-06 17:44:53 +0700

    edit header and main-menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,27 @@
-'use strict';
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var uncss = require('gulp-uncss');
 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+sass = require('gulp-sass'); //Подключаем Sass пакет
+browserSync = require('browser-sync'); // Подключаем Browser Sync
+
+
+gulp.task('sass', function(){ // Создаем таск "sass"
+    return gulp.src('./sass/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(gulp.dest('./dev/css/')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('process-css', function(){
-  gulp.src('css/**/*.css')
-          .pipe(uncss({
-            html: ['index.html']
-        }))
-  .pipe(gulp.dest('css/'));
-	});
-
-
-
-
-
-gulp.task('watch', function() {
-  gulp.watch('sass/*.scss',['sass']);
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: 'dev' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
 });
 
-gulp.task('default', ['process-css', 'sass', 'watch']);
-
-
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('./sass/**/*.scss', ['sass']); // Наблюдение за sass файлами
+    gulp.watch('./dev/css/style.css', browserSync.reload);
+    gulp.watch('./dev/index.html', browserSync.reload);
+});
